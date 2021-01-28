69b47108b92c112c61c1c33307924c354369a8c0
eNorese <enzo.norese@gmail.com>
2021-01-28 01:15:23 -0300

    models created

---
+++ db/migrate/20210128040405_create_vehicles.rb
@@ -0,0 +1,17 @@
+class CreateVehicles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :vehicles do |t|
+      t.references :user, foreign_key: true
+      t.string :type
+      t.string :color
+      t.string :brand
+      t.string :model
+      t.integer :max_speed
+      t.boolean :air_conditioning
+      t.boolean :turbo
+      t.boolean :electric
+
+      t.timestamps
+    end
+  end
+end
